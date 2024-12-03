4f7c1269e79d3fc9b231f23446d7aba02dcf10a6
Tomasz Brengos <iwashis@gmail.com>
2024-12-04 00:47:02 +0100

    fourmolu

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Lib
-    ( tasks
-    ) where
+
+module Lib (
+    tasks,
+) where
+
 import Task1 (task1)
 import Task3 (task3)
 
 tasks :: IO ()
 tasks = do
-  task1
-  task3
+    task1
+    task3
--- src/Task3.hs
+++ src/Task3.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Task3
-    ( task3
-    ) where
+
+module Task3 (
+    task3,
+) where
+
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
 parseIntegers :: Parser (Integer, Integer)
 parseIntegers = do
-  _ <- string "mul("
-  x <- many1 digit
-  _ <- char ','
-  y <- many1 digit
-  _ <- char ')'
-  return (read x, read y)
+    _ <- string "mul("
+    x <- many1 digit
+    _ <- char ','
+    y <- many1 digit
+    _ <- char ')'
+    return (read x, read y)
 
 data DoDontInts = Yes | No | Pair (Integer, Integer)
-  deriving (Show)
+    deriving (Show)
 
 parseDoDonts :: Parser DoDontInts
 parseDoDonts = try (string "do()" >> return Yes) <|> (string "don't()" >> return No)
@@ -27,38 +29,36 @@ parseDoDontsInts = many tryParseOrSkip
     skipInvalid = anyChar >> returnNothing
     returnNothing = return Nothing
 
-
 skipNothing :: [Maybe a] -> [a]
 skipNothing [] = []
-skipNothing (x:xs) = case x of
-  Nothing -> skipNothing xs
-  (Just y) -> y : skipNothing xs
+skipNothing (x : xs) = case x of
+    Nothing -> skipNothing xs
+    (Just y) -> y : skipNothing xs
 
 compute :: [(Integer, Integer)] -> Integer
 compute list = sum $ map (uncurry (*)) list
 
 removeNoYesIntervals :: [DoDontInts] -> [(Integer, Integer)]
-removeNoYesIntervals list  = go list True
+removeNoYesIntervals list = go list True
   where
     go [] _ = []
-    go (x:xs) bool = case x of
-      Yes -> go xs True
-      No  -> go xs False
-      Pair p -> if bool then p : go xs bool else go xs bool
+    go (x : xs) bool = case x of
+        Yes -> go xs True
+        No -> go xs False
+        Pair p -> if bool then p : go xs bool else go xs bool
 
-removeNoYes:: [DoDontInts] -> [(Integer, Integer)]
+removeNoYes :: [DoDontInts] -> [(Integer, Integer)]
 removeNoYes [] = []
-removeNoYes (x:xs)  = case x of
-      Yes -> removeNoYes xs
-      No  -> removeNoYes xs
-      Pair p -> p : removeNoYes xs
-
+removeNoYes (x : xs) = case x of
+    Yes -> removeNoYes xs
+    No -> removeNoYes xs
+    Pair p -> p : removeNoYes xs
 
 task3 :: IO ()
 task3 = do
     let filePath = "input/input3.txt"
     contents <- readFile filePath
-    let concatenated = concat $  lines contents
+    let concatenated = concat $ lines contents
     let list = skipNothing <$> parse parseDoDontsInts "" concatenated
     let subtask1 = removeNoYes <$> list
     print "Task 3. Part 1"
